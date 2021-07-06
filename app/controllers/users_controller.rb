bf77f8e65e1951f66144c1f9b7418cf50d5a3645
Eugenia Amaya <eugenia@estudioamaya.net>
2021-07-06 15:29:39 -0300

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
-def users_params
-  params.require(:user).permit(:email, :full_name, :photo)
+class UsersController < ApplicationController
+  def users_params
+    params.require(:user).permit(:email, :full_name, :photo)
+  end
 end
