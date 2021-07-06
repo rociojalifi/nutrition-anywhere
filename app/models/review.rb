65248218970a025a3d225e12b4bf09f144b57962
Rocio Jalifi <jalifirocio@gmail.com>
2021-07-06 16:14:31 +0200

    adds controllers

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :service
+  
+  validates :rating, presence: true
+  validates :content, presence: true
+  validates :content, lenght: { minimun: 10}
+
 end
