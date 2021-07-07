4720ec5a730f782db8680af61450c53c6adc765a
Rocio Jalifi <jalifirocio@gmail.com>
2021-07-07 20:30:05 +0200

    navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @services = Service.all.shuffle.first(3)
-    
+    @user = current_user
   end
 end
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,21 +1,30 @@
 class ServicesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_service, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @services = policy_scope(Service)
-    @markers = @services.geocoded.map do |service|
-      {
+  
+    def index
+      @services = policy_scope(Service)
+      @markers = @services.geocoded.map do |service|
+        {
         lat: service.latitude,
         lng: service.longitude,
         info_window: render_to_string(partial: "info_window", locals: { service: service }),
         image_url: helpers.asset_url(service.user.photo)
       }
     end
-    if current_user.nutritionist?
-      @services = current_user.services
-    else
-      @services = Service.all
+    
+    if user_signed_in?
+      if current_user.nutritionist? 
+        @services = current_user.services 
+        @markers = @services.geocoded.map do |service|
+          {
+          lat: service.latitude,
+          lng: service.longitude,
+          info_window: render_to_string(partial: "info_window", locals: { service: service }),
+          image_url: helpers.asset_url(service.user.photo)
+        }
+        end
+      end
     end
   end
 
@@ -47,7 +56,7 @@ class ServicesController < ApplicationController
 
   def update 
     if @service.update(service_params)
-    redirect_to services_path
+      redirect_to services_path
     else
       render :edit
     end
@@ -55,7 +64,7 @@ class ServicesController < ApplicationController
 
   def destroy
     if @service.destroy
-    redirect_to services_path(@service)
+      redirect_to services_path(@service)
     else
       render :index
     end
@@ -66,9 +75,9 @@ class ServicesController < ApplicationController
     params.require(:service).permit(:price, :speciality)
   end
 
-  # def user_params
-  #   params[:service].require(:users).permit(:full_name, :photo, :role)
-  # end
+  def user_params
+    params[:service].require(:users).permit(:full_name, :photo, :role)
+  end
 
   def set_service
     @service = Service.find(params[:id])
