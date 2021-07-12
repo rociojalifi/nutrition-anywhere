cca443f9a80e4061c6ef4302f11247cc1da5c74d
Rocio Jalifi <jalifirocio@gmail.com>
2021-07-12 19:25:08 +0200

    fix(settings) Do not use controller method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,4 @@
 class UsersController < ApplicationController
   def settings
   end
-
-  def zoom_auth_url
-    "https://zoom.us/oauth/authorize?client_id=#{ENV['ZOOM_CLIENT_ID']}&response_type=code&redirect_uri=#{ENV['ZOOM_CALLBACK']}"
-  end
 end
